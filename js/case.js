ee99a4a7dc92e416d241501aa82641c479bf5264
indrajith76 <indrajithgoswami@gmail.com>
2022-08-20 01:30:42 +0600

    shopping cart handle decrease and update price

--- js/case.js
+++ js/case.js
@@ -1,21 +1,33 @@
-document.getElementById('btn-case-plus').addEventListener('click', function(event){
+function updateCaseNumber(isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberFieldString = caseNumberField.value;
     const previousCaseNumber = parseInt(caseNumberFieldString);
-    const newCaseNumber = previousCaseNumber + 1;
-    if(newCaseNumber > 10){
-        return;
+    let newCaseNumber;
+    if(isIncrease){
+        newCaseNumber = previousCaseNumber + 1;
+        if(newCaseNumber > 10){
+            return;
+        }
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+        if(newCaseNumber < 1){
+            return;
+        }  
     }
     caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+}
+
+// code for plus button
+document.getElementById('btn-case-plus').addEventListener('click', function(event){
+    const newCaseNumber = updateCaseNumber(true);
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
 });
 
+// code for minus button
 document.getElementById('btn-case-minus').addEventListener('click', function(){
-    const caseNumberField = document.getElementById('case-number-field');
-    const caseNumberFieldString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberFieldString);
-    const newCaseNumber = previousCaseNumber - 1;
-    if(newCaseNumber < 1){
-        return;
-    }
-    caseNumberField.value = newCaseNumber;    
+    updateCaseNumber(false);
 })
